071d074596b41cb87d8ec1d84b9ba0e292a87dc3
ffotaku <68525309+ffotaku@users.noreply.github.com>
2020-09-25 15:40:52 +0700

    enter

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="https://tr.rbxcdn.com/38e2b8adf7d7f18731c5aa4efc442f4a/420/420/Decal/Png">
+    <anime/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import anime from '@/components/anime.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld,
+    anime
   }
 }
 </script>
 
 <style>
+
 #app {
+  background-image: url(https://i.pinimg.com/originals/6b/04/16/6b04166d73e5af48ffecc373135bab7c.jpg);
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
